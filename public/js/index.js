6bf27e3041214906e0d49ffa85138bf1feaea25a
Bhupender Singh <bhupenders225@gmail.com>
2018-10-10 00:24:48 +0530

    navbar responsive

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,26 @@
-var scollAnimation = function () {
-    xAxis = scrollX;
-    yAxis = scrollY;
-    if (yAxis < $('.navbar').innerHeight()) {
-        $('.navbar').removeClass('navActive');
-    } else if (yAxis > $('.navbar').innerHeight()) {
-        $('.navbar').addClass('navActive');
-    }
-}
+ window.addEventListener("scroll", () => {
+     xAxis = scrollX;
+     yAxis = scrollY;
+     if (yAxis < $('.navbar').innerHeight()) {
+         $('.navbar').removeClass('navActive');
+     } else if (yAxis > $('.navbar').innerHeight()) {
+         $('.navbar').addClass('navActive');
+     }
+ });
 
+ $('.navbar-toggler').on('click', () => {
+     $('.navbar').toggleClass('navActiveToggle');
+ })
 
-window.addEventListener("scroll", scollAnimation);
+ // shape dance
+ $(document).ready(() => {
+     var left_shape = $('.shape_left');
+     var right_shape = $('.shape_right');
+     left_shape.toggleClass('rotate_more_l')
+     right_shape.toggleClass('rotate_more_r')
+     var changeit = () => {
+         left_shape.toggleClass('rotate_more_l');
+         right_shape.toggleClass('rotate_more_r');
+     }
+     setInterval(changeit, 5000);
+ });
