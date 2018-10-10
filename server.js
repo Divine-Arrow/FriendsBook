998ccb285b5d0449563a38e7209bf856e7f3119a
Bhupender Singh <bhupenders225@gmail.com>
2018-10-11 01:04:48 +0530

    adding express-handlebars and register page

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express(),
     port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-/* app.get('/', (req, res) => {
-    res.render('index.html');
-}); */
+// view engine
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
 
 
 app.listen(port, () => {
