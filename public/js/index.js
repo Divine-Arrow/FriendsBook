424b0f3b1c5fdd64654d414820ff9fe1aa50b9e8
Bhupender Singh <bhupenders225@gmail.com>
2018-10-10 11:51:19 +0530

    added loader

--- public/js/index.js
+++ public/js/index.js
@@ -24,3 +24,26 @@
      }
      setInterval(changeit, 5000);
  });
+
+
+ /* loader */
+ /* loader animation removal */
+ var clicked = false;
+ var stopScroll = () => {
+     window.scroll(xAxis, yAxis);
+ }
+ var loader = () => {
+     $(".loader_container").css('top', '-100vh');
+     $(".loader_container").css('opacity, 0');
+     clicked = !clicked;
+     window.removeEventListener("scroll", stopScroll);
+ }
+
+ /* loader listener */
+ window.addEventListener("load", () => {
+     xAxis = 0;
+     yAxis = 0;
+     clicked = !clicked;
+     window.addEventListener("scroll", stopScroll);
+     loader();
+ });
