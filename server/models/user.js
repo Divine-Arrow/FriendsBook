1e6ef374e222b1d4e4f51af9e24cdef16efd741e
Bhupender Singh <bhupenders225@gmail.com>
2018-10-25 00:29:40 +0530

    initalizing backend

---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://localhost/friendsBook");
+
+var userSchema = new mongoose.Schema({
+    mail: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        minlength: 6
+    }
+}); 
+
+
+const users = mongoose.model('users', 'userSchema');
+
+module.exports = users;
