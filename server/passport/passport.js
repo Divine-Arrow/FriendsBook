0fab96c3f6eb1a3c02b60f33fb0e5d4a1a41b4b2
Bhupender Singh <bhupenders225@gmail.com>
2018-11-02 12:48:02 +0530

    err 2

--- server/passport/passport.js
+++ server/passport/passport.js
@@ -168,17 +168,20 @@ passport.use(new FacebookStrategy({
         email: profile._json.email
     }).then((user) => {
         if (!user) {
-            var userData = {
-                email: profile._json.email,
-                name: profile._json.first_name,
-                lastName: profile._json.last_name,
-                birthday: profile._json.birthday,
-                fImage: profile._json.picture.data.url,
-                gender: profile._json.gender,
-                facebookId: profile._json.id,
-                hometown: profile._json.hometown.name,
-                location: profile._json.location.name
-            };
+            var userData;
+            try {
+                userData = {
+                    email: profile._json.email,
+                    name: profile._json.first_name,
+                    lastName: profile._json.last_name,
+                    birthday: profile._json.birthday,
+                    fImage: profile._json.picture.data.url,
+                    gender: profile._json.gender,
+                    facebookId: profile._json.id,
+                    hometown: profile._json.hometown.name,
+                    location: profile._json.location.name
+                };
+            }
             var newFUser = new User(userData);
             newFUser.save().then((savedFUser) => {
                 if (savedFUser)
