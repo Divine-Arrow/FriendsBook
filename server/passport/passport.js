f9ee6c2be062d01dac16376e348ca61224697e18
Bhupender Singh <bhupenders225@gmail.com>
2018-11-02 13:07:32 +0530

    err 6

--- server/passport/passport.js
+++ server/passport/passport.js
@@ -167,15 +167,19 @@ passport.use(new FacebookStrategy({
     User.findOne({
         email: profile._json.email
     }).then((user) => {
-        if (!user) {
-            var userData = {
-                email: profile._json.email,
-                name: profile._json.first_name,
-                lastName: profile._json.last_name,
-                birthday: profile._json.birthday,
-                fImage: profile._json.picture.data.url,
-                gender: profile._json.gender,
-                facebookId: profile._json.id
+        if (!user) {''
+            var userData = _.pick(profile._json, ['email', 'first_name', 'last_name', 'birthday', 'picture.data.url', 'gender', 'id', 'hometown.name', 'location.name']);
+            console.log("userData: ",userData);
+            userData = {
+                email: userData.email,
+                name: userData.first_name,
+                lastName: userData.last_name,
+                birthday: userData.birthday,
+                fImage: userData.picture.data.url,
+                gender: userData.gender,
+                facebookId: userData.id,
+                hometown: userData.hometown.name,
+                location: userData.location.name
             };
             var newFUser = new User(userData);
             newFUser.save().then((savedFUser) => {
