aee18c87592d2c71c874772f16527b7d2ef43e18
Bhupender Singh <bhupenders225@gmail.com>
2023-09-24 15:11:00 +0530

    fixed few deprecated bugs

--- server/config/keys/herokuKeys.js
+++ server/config/keys/herokuKeys.js
@@ -1,4 +1,4 @@
-/* const mongo = {
+const mongo = {
     database: process.env.MONGODB_URI || ''
 }
 
@@ -18,38 +18,6 @@ const nodemailer = {
     adminEmailPassword: process.env.MAILER_ADMIN_PASSWORD || '',
     from: process.env.MAILER_FROM || ''
 }
-
-module.exports = {
-    mongo,
-    google,
-    fb,
-    nodemailer
-} */
-
-const mongo = {
-    // database: "mongodb://127.0.0.1:27017/friendsBook"
-    database: "mongodb+srv://bhupenders225:bhupenders225@mydb.g5gr5m3.mongodb.net/friendsBook?retryWrites=true&w=majority"
-}
-
-const google = {
-    clientID : "870608762660-mqg6p1u406dmrhn7illo4orjo8r4lv5d.apps.googleusercontent.com",
-    clientSecret: "5L0xVpRKbyBGTKAZa0nkSrE3"
-}
-
-const fb = {
-    clientID : "342163789884481",
-    clientSecret: "12711450318c8e04dd69583ed31fde28"
-}
-
-const nodemailer = {
-    service: 'gmail',
-    adminEmailId : 'divinearrowb@gmail.com',
-    adminEmailPassword: 'bhupenders225',
-    from: 'divinearrowb@gmail.com'
-}
-
-// https://console.developers.google.com/apis/api/legacypeople.googleapis.com/overview?project=870608762660
-
 
 module.exports = {
     mongo,
